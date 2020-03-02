ed322bc467c476204eb251991652e0fda0fd510c
jmills1990 <47228413+jmills1990@users.noreply.github.com>
2020-03-01 21:24:02 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,52 @@
+
+
+function updateItemStatus(){
+    var cbID = this;
+    alert(cbID);
+
+}
+
+
+function addNewItem(list, itemText) {
+    totalItems++;
+
+    var listItem = document.createElement("li");
+    var checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.id = "cb_" + totalItems;
+    checkBox.onclick = updateItemStatus; 
+
+
+    var span = document.createElement("span");
+    span.id = "item_" + totalItems;
+    span.innerText = itemText;
+
+    listItem.appendChild(checkBox);
+    listItem.appendChild(span);
+
+    list.appendChild(listItem);
+
+}
+var totalItems = 0;
+var inItemText = document.getElementById("inItemText");
+inItemText.focus();
+inItemText.onkeyup = function (event) {
+
+    // Event.which (13) -> Enter
+
+if (event.which == 13) {
+ var itemText = inItemText.value
+
+    if (!itemText || itemText == "" || itemText == " ")  {
+    return false;
+}
+
+addNewItem(document.getElementById("todoList"), itemText);
+
+
+//inItemText.select();
+document.getElementById("inItemText").blur();
+document.getElementById("inItemText").value = "";
+inItemText.focus();
+}
+}
