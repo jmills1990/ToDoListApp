8e1d34553c4660077d8e44973cb1eec0b24a1ddd
jmills1990 <47228413+jmills1990@users.noreply.github.com>
2020-03-03 21:59:15 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,25 +1,53 @@
 
 
-function updateItemStatus(){
-    var cbID = this;
-    alert(cbID);
+function updateItemStatus() {
+    var cbID = this.id.replace("cb_", "");
+    var itemText = document.getElementById("item_" + cbID);
 
+    if (this.checked) {
+
+        itemText.className = "checked";
+
+    }
+    else {
+        itemText.className = "";
+
+    }
 }
 
+function renameItem() {
+    // this == span
+    var newText = prompt("What should this item be renamed to?")
 
+    if (!newText || newText == "" || newText == " ") {
+        return false;
+    }
+    this.innerText = newText;
+}
+function removeItem() {
+    //this == span
+    this.style.display = "none"
+
+}
 function addNewItem(list, itemText) {
-    totalItems++;
+    var date = new Date();
+    var id = "" + date.getHours() + date.getMinutes() + date.getSeconds() + date.getMilliseconds();
 
     var listItem = document.createElement("li");
+    listItem.id = "li_" + id;
+    listItem.ondblclick = removeItem;
+
     var checkBox = document.createElement("input");
     checkBox.type = "checkbox";
-    checkBox.id = "cb_" + totalItems;
-    checkBox.onclick = updateItemStatus; 
+    checkBox.id = "cb_" + id;
+    checkBox.onclick = updateItemStatus;
 
 
     var span = document.createElement("span");
-    span.id = "item_" + totalItems;
+    span.id = "item_" + id;
     span.innerText = itemText;
+    span.onclick = renameItem;
+
 
     listItem.appendChild(checkBox);
     listItem.appendChild(span);
@@ -27,26 +55,25 @@ function addNewItem(list, itemText) {
     list.appendChild(listItem);
 
 }
-var totalItems = 0;
 var inItemText = document.getElementById("inItemText");
 inItemText.focus();
 inItemText.onkeyup = function (event) {
 
     // Event.which (13) -> 
 
-if (event.which == 13) {
- var itemText = inItemText.value
+    if (event.which == 13) {
+        var itemText = inItemText.value
 
-    if (!itemText || itemText == "" || itemText == " ")  {
-    return false;
-}
+        if (!itemText || itemText == "" || itemText == " ") {
+            return false;
+        }
 
-addNewItem(document.getElementById("todoList"), itemText);
+        addNewItem(document.getElementById("todoList"), itemText);
 
 
-//inItemText.select();
-document.getElementById("inItemText").blur();
-document.getElementById("inItemText").value = "";
-inItemText.focus();
-}
+        //inItemText.select();
+        document.getElementById("inItemText").blur();
+        document.getElementById("inItemText").value = "";
+        inItemText.focus();
+    }
 }
